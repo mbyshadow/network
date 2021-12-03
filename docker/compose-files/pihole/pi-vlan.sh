9405169518ad0d14ac15956a86ba144f1a97ba64
mbyshadow <mbyshadow@protonmail.com>
2021-12-03 16:30:05 +0100

    gfgf

--- docker/compose-files/pihole/pi-vlan.sh
+++ docker/compose-files/pihole/pi-vlan.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ip link add macvlan-shim link eth0 type macvlan mode bridge
-ip addr add 172.19.16.60/28 dev macvlan-shim
+ip addr add 172.19.32.60/28 dev macvlan-shim
 ip link set macvlan-shim up
 ifconfig macvlan-shim
